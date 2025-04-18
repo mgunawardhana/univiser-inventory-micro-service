a28e7e98e63b2740295ed4392a4fa8aa265783bb
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-04-18 15:44:37 +0530

    controller endpoint implemented

--- src/main/java/com/univiser/inventory/inventory_management/controller/InventoryController.java
+++ src/main/java/com/univiser/inventory/inventory_management/controller/InventoryController.java
@@ -1,9 +1,14 @@
 package com.univiser.inventory.inventory_management.controller;
 
+import com.univiser.inventory.domain.APIResponse;
+import com.univiser.inventory.domain.entity.Item;
+import com.univiser.inventory.inventory_management.repository.InventoryRepo;
+import com.univiser.inventory.inventory_management.service.InventoryService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -11,5 +16,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InventoryController {
 
+    @NonNull
+    private final InventoryService inventoryService;
+
+    @PostMapping
+    public ResponseEntity<APIResponse> createItem(@RequestBody Item item) {
+        log.info("Creating new inventory item {}", item);
+        var response = inventoryService.createItem(item);
+        log.info("Successfully created inventory item {}", item);
+        return response;
+    }
+
+    @PutMapping
+    public ResponseEntity<APIResponse> updateItem(@RequestBody Item item) {
+        log.info("Updating inventory item {}", item);
+        var response = inventoryService.updateItem(item);
+        log.info("Successfully updated inventory item {}", item);
+        return response;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<APIResponse> deleteItem(@RequestBody Item item) {
+        log.info("Deleting inventory item {}", item);
+        var response = inventoryService.deleteItem(item);
+        log.info("Successfully deleted inventory item {}", item);
+        return response;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse> getItemById(@PathVariable Long id) {
+        log.info("Fetching inventory item by ID {}", id);
+        var response = inventoryService.getItemById(id);
+        log.info("Successfully fetched inventory item by ID {}", id);
+        return response;
+    }
+
+    @GetMapping("/fetch-all")
+    public ResponseEntity<APIResponse> getAllItems() {
+        log.info("Fetching all inventory items");
+        var response = inventoryService.getAllItems();
+        log.info("Successfully fetched all inventory items");
+        return response;
+    }
 
 }
